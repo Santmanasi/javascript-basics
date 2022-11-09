43f3e35e37ddc0f2eec02561d92a34bf53f25bfb
Manasi M. Sant <manasisant.27@gmail.com>
2022-11-09 20:20:14 +0530

    search number match

---
+++ searchnumber.js
@@ -0,0 +1,11 @@
+function array() {
+  var arr = [12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32];
+  var i;
+  var num = 22;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] == num) {
+      console.log(arr[i]);
+    }
+  }
+}
+array();
